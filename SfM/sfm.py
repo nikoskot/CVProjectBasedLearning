450550fc3d5e500e4d82a7ff5594f0cc5a5323a0
Nikolaos Kotarelas <nikoskot1@gmail.com>
2026-02-04 23:47:15 +0200

    Create 3d points of first 2 images

--- SfM/sfm.py
+++ SfM/sfm.py
@@ -11,6 +11,7 @@ import datetime
 from tqdm import tqdm
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 from frame import Frame
+from point3d import Point3d
 
 
 def getParser():
@@ -127,12 +128,13 @@ def sfm(images, K, P):
     points3d = []   # The 3d points that are created in the scene. List of Point3d objects
     matches = [[None for _ in range(len(images))] for _ in range(len(images))]      # 2D array that contatins the feature matches between all images. It contains tuples of the coordinates of the feature in the first image the coordinates in the second image
     matchesKeypointsIdxs = [[None for _ in range(len(images))] for _ in range(len(images))]     # Same as the above but it contatins the indexes of the keypoints, not their coordinates
-    
+    projections = []
+     
     rr.log("/camera", rr.Pinhole(resolution=[images[0].shape[1], images[0].shape[0]], focal_length=[K[0,0], K[1,1]], principal_point=[K[0, 2], K[1, 2]]), static=True)
     
     # Calculate keypoints and descriptors for all images
     print("Calculate keypoints and descriptors for all images")
-    featureDetector = cv.ORB_create(3000)
+    featureDetector = cv.ORB_create(5000)
     for imgIdx, img in tqdm(enumerate(images)):
         keypoints, descriptors = featureDetector.detectAndCompute(img, None)
         frame = Frame(img, imgIdx, keypoints, descriptors)
@@ -176,6 +178,10 @@ def sfm(images, K, P):
     poses.append(formTransformation(np.eye(3), np.zeros(3)))
     rr.set_time("frameId", sequence=0)
     rr.log("/camera", rr.Transform3D(mat3x3=poses[0][:3,:3], translation=poses[0][:3,3]))
+    rr.log("/annotatedImages", rr.Image(cv.drawKeypoints(images[0], frames[0].keypoints, 0, (0, 0, 255), flags=cv.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)))
+    
+    # Projection matrix of the 1st image
+    projections.append(P)
     
     # Estimate Essential matrix for 1st and 2nd frame
     E, mask = cv.findEssentialMat(matches[0][1][0], matches[0][1][1], K)
@@ -187,6 +193,27 @@ def sfm(images, K, P):
     poses.append(np.matmul(poses[0], np.linalg.inv(relativePoseTransform)))
     rr.set_time("frameId", sequence=1)
     rr.log("/camera", rr.Transform3D(mat3x3=poses[1][:3,:3], translation=poses[1][:3,3]))
+    rr.log("/annotatedImages", rr.Image(cv.drawKeypoints(images[1], frames[1].keypoints, 0, (0, 0, 255), flags=cv.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)))
+    # Projection matrix of the 2nd image
+    projections.append(np.concatenate((K, np.zeros((3,1))), axis = 1) @ poses[1])
+    
+    # Get the 3D points from the first two images
+    X_h = cv.triangulatePoints(projections[0], projections[1], matches[0][1][0].T, matches[0][1][1].T)
+    X = X_h[:3, :] / X_h[3, :]
+    for i in range(X.shape[1]):
+        # Create a point3d object for the 3d point
+        point3dIdx = len(points3d) + 1
+        point3d = Point3d(X[:, i], point3dIdx)
+        # Add the id of the frames and the keypoints that the 3d point is observed at
+        point3d.observedAt.append((0, matchesKeypointsIdxs[0][1][0][i]))
+        point3d.observedAt.append((1, matchesKeypointsIdxs[0][1][1][i]))
+        # Add the id of the keypoint and the 3d point at the frames' observations
+        frames[0].observations.append((matchesKeypointsIdxs[0][1][0][i], point3dIdx))
+        frames[1].observations.append((matchesKeypointsIdxs[0][1][1][i], point3dIdx))
+        
+        points3d.append(point3d)
+    
+    rr.log("/pointCloud/points", rr.Points3D([p.coords3d for p in points3d], colors=[255, 255, 255, 255], radii=0.01), static=True)
     
     pass
     
